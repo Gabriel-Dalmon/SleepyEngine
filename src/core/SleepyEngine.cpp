811cd842e5922102de05667a59e6e2dd62a2a9fa
Gabriel <dalmgabi@gmail.com>
2024-02-27 12:12:15 +0100

    Update EngineApp initialization

--- src/core/SleepyEngine.cpp
+++ src/core/SleepyEngine.cpp
@@ -3,8 +3,21 @@
 
 #include "framework.h"
 #include "SleepyEngine.h"
+#include <windows.h>
+#include <wrl.h> //ComPtr
+#include <dxgi1_4.h>
+
+#include <d3d12.h>
+#include <cassert>
+
+#include <DXGI.h>
 
 #define MAX_LOADSTRING 100
+
+// Link necessary d3d12 libraries.
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
@@ -16,6 +29,158 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+typedef struct DXGI_MODE_DESC
+{
+    UINT Width; // Buffer resolution width
+    UINT Height; // Buffer resolution height
+    DXGI_RATIONAL RefreshRate;
+    DXGI_FORMAT Format; // Buffer display format
+    DXGI_MODE_SCANLINE_ORDER ScanlineOrdering; //Progressive vs. interlaced
+    DXGI_MODE_SCALING Scaling; // How the image is stretched 
+    // over the monitor.
+} DXGI_MODE_DESC;
+
+typedef struct DXGI_SWAP_CHAIN_DESC
+{
+    DXGI_MODE_DESC BufferDesc;
+    DXGI_SAMPLE_DESC SampleDesc;
+    DXGI_USAGE BufferUsage;
+    UINT BufferCount;
+    HWND OutputWindow;
+    BOOL Windowed;
+    DXGI_SWAP_EFFECT SwapEffect;
+    UINT Flags;
+} DXGI_SWAP_CHAIN_DESC;
+
+class SleepyEngine
+{
+public:
+    SleepyEngine();
+    int Initialize();
+    int Run();
+private:
+    void EnableAdditionalD3D12Debug();
+    void CreateDevice();
+    void CreateFence();
+    void RecoverDescriptorsSize();
+    void Check4xMSAAQualitySupport();
+    void CreateCommandObjects();
+    void CreateSwapChain();
+private:
+    ID3D12Device* m_pDevice;
+    ID3D12Fence* m_pFence;
+
+    UINT m_rtvDescriptorSize = 0;
+    UINT m_dsvDescriptorSize = 0;
+    UINT m_cbvSrvUavDescriptorSize = 0;
+
+    DXGI_FORMAT m_backBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+    UINT m_4xMsaaQuality = 0;
+    bool m_4xMsaaState = false;
+
+    ID3D12CommandQueue* m_pCommandQueue;
+    ID3D12CommandAllocator* m_pDirectCmdListAlloc;
+    ID3D12CommandAllocator* m_pDirectCmdListAlloc;
+    ID3D12GraphicsCommandList* m_pCommandList;
+
+    IDXGIFactory4* m_pDxgiFactory;
+    IDXGISwapChain* m_pSwapChain;
+
+    int m_swapChainBufferCount = 2 //2 for double buffering
+
+    int m_clientWidth;
+    int m_clientHeight;
+
+    HWND mhMainWnd = nullptr;
+};
+
+void SleepyEngine::EnableAdditionalD3D12Debug()
+{
+    Microsoft::WRL::ComPtr<ID3D12Debug> debugController;
+    D3D12GetDebugInterface(__uuidof(ID3D12Device), &debugController); //should throw error
+    debugController->EnableDebugLayer();
+}
+
+void SleepyEngine::CreateDevice()
+{
+    D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), (void**)&m_pDevice);
+    //if failed, check book for "WARP_Adapters".
+
+}
+
+void SleepyEngine::CreateFence()
+{
+    int iResult;
+    iResult = m_pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, __uuidof(ID3D12Device), (void**)&m_pFence);
+}
+
+void SleepyEngine::RecoverDescriptorsSize()
+{
+    m_rtvDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    m_dsvDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+    m_cbvSrvUavDescriptorSize = m_pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+void SleepyEngine::Check4xMSAAQualitySupport()
+{
+    int iResult;
+    D3D12_FEATURE_DATA_MULTISAMPLE_QUALITY_LEVELS msQualityLevels; //will likely be moved to private member attribute
+    msQualityLevels.Format = m_backBufferFormat;
+    msQualityLevels.SampleCount = 4;
+    msQualityLevels.Flags = D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE;
+    msQualityLevels.NumQualityLevels = 0;
+    iResult = m_pDevice->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS, &msQualityLevels, sizeof(msQualityLevels));
+    assert(m_4xMsaaQuality > 0 && "Unexpected MSAA quality level.");
+}
+
+void SleepyEngine::CreateCommandObjects()
+{
+    int iResult;
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    iResult = m_pDevice->CreateCommandQueue(&queueDesc, __uuidof(ID3D12Device), (void**) &m_pCommandQueue);
+    iResult = m_pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, __uuidof(ID3D12Device), (void**)&m_pDirectCmdListAlloc);
+    iResult = m_pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_pDirectCmdListAlloc, nullptr, __uuidof(ID3D12Device), (void**)&m_pCommandList);
+    m_pCommandList->Close();
+}
+
+void SleepyEngine::CreateSwapChain()
+{
+    delete m_pSwapChain;
+    DXGI_SWAP_CHAIN_DESC swapChainDescriptor;
+    swapChainDescriptor.BufferDesc.Width = m_clientWidth;
+    swapChainDescriptor.BufferDesc.Height = m_clientHeight;
+    swapChainDescriptor.BufferDesc.RefreshRate.Numerator = 60;
+    swapChainDescriptor.BufferDesc.RefreshRate.Denominator = 1;
+    swapChainDescriptor.BufferDesc.Format = m_backBufferFormat;
+    swapChainDescriptor.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+    swapChainDescriptor.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+    swapChainDescriptor.SampleDesc.Count = m_4xMsaaState ? 4 : 1;
+    swapChainDescriptor.SampleDesc.Quality = m_4xMsaaState ? (m_4xMsaaQuality - 1) : 0;
+    swapChainDescriptor.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDescriptor.BufferCount = SwapChainBufferCount;
+    swapChainDescriptor.OutputWindow = mhMainWnd;
+    swapChainDescriptor.Windowed = true;
+    swapChainDescriptor.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDescriptor.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+    m_pDxgiFactory->CreateSwapChain(m_pCommandList, &swapChainDescriptor, &m_pSwapChain)
+}
+
+int SleepyEngine::Initialize() 
+{
+    #if defined(DEBUG) || defined(_DEBUG)
+    EnableAdditionalD3D12Debug();
+    #endif
+    CreateDevice();
+    CreateFence();
+    RecoverDescriptorsSize();
+    Check4xMSAAQualitySupport();
+    CreateCommandObjects();
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
