865b933181c8ec2ea77316ce395ded39c48c254d
Kerly <115028766+yoannklt@users.noreply.github.com>
2024-02-27 09:55:50 +0100

    updated Entity class

--- headers/Entity.h
+++ headers/Entity.h
@@ -10,12 +10,25 @@ public:
 	~Entity() {};
 
 	// INIT
-	void Init();
+	// void Init();
 
 	// SETTER / GETTER
 	void AddComponent(Component* component);
-	void RemoveComponent(Component* component);
 
+	template<typename T>
+	void RemoveComponent()
+	{
+		for (int i = 0; i < m_componentsList.size(); i++)
+		{
+			if (m_componentsList[i] == T)
+			{
+				m_componentsList.erase(m_componentsList.begin() + i);
+			}
+
+			return;
+		}
+	}
+	
 	template<typename T>
 	T GetComponent() {
 		for (int i = 0; i < m_componentsList.size(); i++)
@@ -23,6 +36,10 @@ public:
 			// TO DO: Code pour trouver le bon component dans m_componentsList
 			// si trouvé -> le retourner 
 			// sinon en bas
+			if (m_componentsList[i] == T)
+			{
+				return m_componentsList[i];
+			}
 		}
 		return nullptr;
 	}
--- src/core/Entity.cpp
+++ src/core/Entity.cpp
@@ -1,1 +1,24 @@
 #include "Entity.h"
+
+Entity::Entity()
+{
+}
+
+void Entity::AddComponent(Component* component)
+{
+	for (int i = 0; i < m_componentsList.size(); i++)
+	{
+		if (m_componentsList[i] == component)
+			return;
+	}
+	m_componentsList.push_back(component);
+}
+
+void Entity::Clear()
+{
+	for (int i = 0; i < m_componentsList.size(); i++)
+	{
+		m_componentsList.erase(m_componentsList.begin() + i);
+	}
+}
+
